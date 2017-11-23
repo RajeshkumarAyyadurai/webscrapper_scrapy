00d1e0e812c9d8aa24a91d99faed37d240fa3aac
Rajeshkumar Ayyadurai <rajeshkumar.ayyadurai@neustar.biz>
2017-11-23 23:16:47 +0530

    adding setup.py and checkjobs.py files

--- bases/selectors/Selectors.py
+++ bases/selectors/Selectors.py
@@ -2,4 +2,4 @@ KupatanaDict = dict(
     DIV_ProductDescriptionBox = '//div[@class="content-wrapper"]',
     DIV_GetClassName = 'div[1]/@class',
     REGEX_GetProdID = "list-item-show_number\s*(.*)",
-)
+)   
